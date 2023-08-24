847bad40544230168c2132c9977438a00538c6ce
Davi Senra <davi.senra98@gmail.com>
2023-08-24 09:59:28 -0300

    fix: removed stat component description

--- web/components/Dashboard/Stat.vue
+++ web/components/Dashboard/Stat.vue
@@ -18,6 +18,6 @@ defineProps({
     <div class="w-full rounded p-3 shadow-md">
         <p class="text-lg font-bold">{{ title }}</p>
         <p class="text-3xl font-extrabold">{{ stat }}</p>
-        <p class="text-sm" v-if="description">{{ description }}% of total bookmarks</p>
+        <p class="text-sm" v-if="description">{{ description }}</p>
     </div>
 </template>
